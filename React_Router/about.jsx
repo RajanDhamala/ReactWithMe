755168e204154b2216a3125fabbe067b4293b685
Rajan Dhamala <157113220+RajanDhamala@users.noreply.github.com>
2024-07-21 21:16:02 +0545

    Create about.jsx

---
+++ React_Router/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+        <h1 style={{backgroundColor:"red"}}>chalo muje contact kero</h1>
+        <button className="bg-red-500">click me</button>
+        </>
+    )
+    }
+export default About
