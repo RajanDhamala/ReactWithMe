80e12e689fa04f48be730b4c7a65400c7190af4d
Rajan Dhamala <157113220+RajanDhamala@users.noreply.github.com>
2024-07-22 21:33:03 +0545

    Create App.jsx

---
+++ React_Router/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './components/home'
+import Login from './components/login'
+import About from './components/about'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+  <BrowserRouter>
+  <h1>header hu meh</h1>
+  <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='login/' element={<Login/>}></Route>
+      <Route path='about/' element={<About/>}></Route>
+      <Route path='navbar/' element={<Navbar/>}></Route>
+  </Routes>
+  <h1>footer hu meh betwa</h1>
+  </BrowserRouter>
+  )
+}
+export default App
