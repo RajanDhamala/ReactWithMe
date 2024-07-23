cce1348eacdade0c0707ddc0b6ed8095c5ab22bd
Rajan Dhamala <157113220+RajanDhamala@users.noreply.github.com>
2024-07-23 19:03:26 +0545

    Update about.jsx

--- React_Router/about.jsx
+++ React_Router/about.jsx
@@ -4,7 +4,7 @@ function About(){
     return(
         <>
         <h1 style={{backgroundColor:"red"}}>chalo muje contact kero</h1>
-        <button className="bg-red-500">click me</button>
+        <button">click me</button>
         </>
     )
     }
