c8433de7493e06e6b498ef6a2dd39d7b785f5388
Rajan Dhamala <157113220+RajanDhamala@users.noreply.github.com>
2024-07-29 21:11:19 +0545

    Update Shopping_Cart.jsx

--- Daraz_Componet/Shopping_Cart.jsx
+++ Daraz_Componet/Shopping_Cart.jsx
@@ -10,19 +10,22 @@ function Rating({ rating }) {
     return <div>{stars}</div>;
   }
 
-  function Shoppingcart({ image, productname = "USB 2.0 to RJ45 Wired Lan Ethernet NEtwork Adapater", ratingObtained, itemsSold,Price,Discountprice="",Productlink}) {
+  function Shoppingcart({ image, productname, ratingObtained, itemsSold,Price,Discountprice="",Productlink}) {
     return (<>
       <div className="w-52 delay-200 hover:text-blue-500 hover:shadow-2xl hover:cursor-pointer">
         <img src={image} alt="" className="h-52" />
         <h1 className="cursor-pointer pl-2 mr-3 text-sm font-sans mt-2">{productname}...</h1>
         <div className="flex items-center ml-2">
           <Rating rating={ratingObtained} />
-          <span className="ml-2 text-gray-500">{itemsSold} items sold</span>
+          {itemsSold!==""&&(
+          <span className="ml-2 text-gray-500">{itemsSold} items sold</span>)}
         </div>
         <Freedelivery link={Productlink}/>
       <div className="flex flex-row gap-2 pb-3">
         <span className="ml-2 text-orange-600 text-[22px]">{'Rs. '+ Price}</span>
-        <del className="text-gray-500 text-[12px] mt-[9px]">{'Rs. '+Discountprice}</del>
+        {Discountprice !=="" ?(
+        <del className="text-gray-500 text-[12px] mt-[9px]">{'Rs.'+Discountprice}</del>):
+        <span className="text-gray-500 text-[12px] mt-[9px]">Discount Soon</span>}
       </div>
       </div>
       </>
